521a0e3f0caaca1e15b0c9ca3d7d5c11ee6ccaab
Marta Mularczyk <mulmarta@amazon.ch>
2023-03-03 16:45:41 +0100

    cleanup

--- interop/scenario_generators/test.py
+++
@@ -1,9 +0,0 @@
-from group import *
-
-g = Group(1, "Basic Run")
-
-g.propose_and_commit(0, [('add', 0, None) for _ in range(10000)])
-
-
-with open('config.json', 'w') as f:
-    f.write(get_json([g]))
